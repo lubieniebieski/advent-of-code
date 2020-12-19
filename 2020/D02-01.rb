e0aeb6fe0f9a8b01da7b9372ec695d0432c2d1ba
Adam Nowak <adam@netguru.com>
2020-12-19 23:07:58 +0100

    D02

---
+++ 2020/D02-01.rb
@@ -0,0 +1,17 @@
+require_relative 'lib/utils.rb'
+
+class Calculation
+  def initialize
+    @passwords = IO.readlines('./D02-input')
+  end
+
+  def run
+    @passwords.find_all do |p|
+      capture = p.match(/(?<min>\d+)-(?<max>\d+)\s(?<letter>\D):\s(?<string>\S+)/)
+      count = capture['string'].chars.count { |c| c == capture["letter"]}
+      (capture['min'].to_i..capture['max'].to_i).cover?(count)
+    end.size
+  end
+end
+
+Measure.new('D02-01', Calculation.new).run
---
+++ 2020/D02-02.rb
@@ -0,0 +1,18 @@
+require_relative 'lib/utils.rb'
+
+class Calculation
+  def initialize
+    @passwords = IO.readlines('./D02-input')
+  end
+
+  def run
+    @passwords.find_all do |p|
+      capture = p.match(/(?<yes_index>\d+)-(?<no_index>\d+)\s(?<letter>\D):\s(?<string>\S+)/)
+
+    (capture['string'].chars[capture['yes_index'].to_i - 1] == capture['letter']) ^
+      (capture['string'].chars[capture['no_index'].to_i - 1] == capture['letter'])
+    end.size
+  end
+end
+
+Measure.new('D02-02', Calculation.new).run

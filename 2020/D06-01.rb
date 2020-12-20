2f6e726c27e25cf74a870b2749188b1de5d29daf
Adam Nowak <adam@netguru.com>
2020-12-20 14:50:33 +0100

    D06

---
+++ 2020/D06-01.rb
@@ -0,0 +1,23 @@
+require_relative 'lib/utils.rb'
+
+class Calculation
+  def initialize
+    @groups = IO.read('./D06-input').split("\n\n").map { |g| Group.new(g) }
+  end
+
+  def run
+    @groups.sum { |g| g.sum_of_yeses }
+  end
+end
+
+class Group
+  def initialize(data)
+    @raw_data = data
+  end
+
+  def sum_of_yeses
+    @raw_data.split("\n").join.chars.uniq.count
+  end
+end
+
+Measure.new('D06-01', Calculation.new).run
---
+++ 2020/D06-02.rb
@@ -0,0 +1,23 @@
+require_relative 'lib/utils.rb'
+
+class Calculation
+  def initialize
+    @groups = IO.read('./D06-input').split("\n\n").map { |g| Group.new(g) }
+  end
+
+  def run
+    @groups.sum { |g| g.common_yeses }
+  end
+end
+
+class Group
+  def initialize(data)
+    @raw_data = data
+  end
+
+  def common_yeses
+    @raw_data.split("\n").map(&:chars).inject(:&).count
+  end
+end
+
+Measure.new('D06-02', Calculation.new).run

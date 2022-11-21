0e69829112fd30a366a7ab8faca69e1b6d8d9771
Adam Nowak <adam.nowak@netguru.com>
2022-11-21 21:09:07 +0000

    d01 complete

--- 2021-golang/01/01.go
+++ 2021-golang/01/01.go
@@ -7,22 +7,45 @@ import (
 )
 
 func PartOne(nums []int) (int, error) {
-  increasedCounter := 0
-  for i := 1; i < len(nums); i++ {
-    if nums[i] > nums[i-1] {
-      increasedCounter++
-    }
-  }
-  return increasedCounter, nil
+	increasedCounter := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > nums[i-1] {
+			increasedCounter++
+		}
+	}
+	return increasedCounter, nil
+}
+
+func PartTwo(nums []int) (int, error) {
+	increasedCounter := 0
+	i := 2
+	previousWindowSum := nums[i-2] + nums[i-1] + nums[i]
+	for i := 3; i < len(nums); i++ {
+
+		windowSum := nums[i-2] + nums[i-1] + nums[i]
+		if windowSum > previousWindowSum {
+			increasedCounter++
+		}
+		previousWindowSum = windowSum
+	}
+	return increasedCounter, nil
 }
 
 func main() {
-  nums := tools.ParseIntegersFromFile("input.txt")
-  answer, err := PartOne(nums)
+	nums := tools.ParseIntegersFromFile("input.txt")
+	answer, err := PartOne(nums)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  fmt.Printf("Part One: %d \n", answer)
+	fmt.Printf("Part One: %d \n", answer)
+
+	answer, err = PartTwo(nums)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Part Two: %d \n", answer)
 }
--- 2021-golang/01/01_test.go
+++ 2021-golang/01/01_test.go
@@ -17,9 +17,31 @@ func TestPartOne(t *testing.T) {
   269
   260
   263
-`
+  `
 	want := 7
 	got, _ := PartOne(tools.ParseIntegersFromString(input))
+	compareWithExample(t, want, got)
+}
+
+func TestPartTwo(t *testing.T) {
+	input := `199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263
+  `
+	want := 5
+	got, _ := PartTwo(tools.ParseIntegersFromString(input))
+	compareWithExample(t, want, got)
+}
+
+func compareWithExample(t *testing.T, want, got int) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("According to the example, we should have %d, but we've got %d", want, got)

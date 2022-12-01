19653e11ab88cb82ca95e189451cea9c9a3e445d
Adam Nowak <adam.nowak@netguru.com>
2022-12-01 13:17:39 +0100

    skeleton for 2022 in golang

---
+++ 2022-golang/tools/tools.go
@@ -0,0 +1,36 @@
+package tools
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ParseIntegersFromFile(fileName string) (result []int) {
+	input, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	return ExtractIntegersFromString(string(input))
+}
+
+func ExtractIntegersFromString(input string) (ans []int) {
+	for _, l := range strings.Split(input, "\n") {
+
+		number, _ := strconv.Atoi(strings.TrimSpace(l))
+		if number == 0 {
+			continue
+		}
+		ans = append(ans, int(number))
+	}
+	return ans
+}
+
+func ExtractStringsFromString(input string) (result []string) {
+
+	for _, l := range strings.Split(input, "\n") {
+		result = append(result, strings.TrimSpace(l))
+	}
+	return result
+}
+

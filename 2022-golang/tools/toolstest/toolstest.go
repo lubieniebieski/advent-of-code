7a4f63de5ae7df9a6aacca8afa0942c38591f582
Adam Nowak <adam.nowak@netguru.com>
2022-12-05 08:34:19 +0100

    use interface for tests comparison

--- 2022-golang/tools/toolstest/toolstest.go
+++ 2022-golang/tools/toolstest/toolstest.go
@@ -2,10 +2,10 @@ package toolstest
 
 import "testing"
 
-func CompareWithExample(t *testing.T, want, got int) {
+func CompareWithExample(t *testing.T, want, got interface{}) {
 	t.Helper()
 
 	if got != want {
-		t.Errorf("According to the example, we should have %d, but we've got %d", want, got)
+		t.Errorf("According to the example, we should have %v, but we've got %v", want, got)
 	}
 }

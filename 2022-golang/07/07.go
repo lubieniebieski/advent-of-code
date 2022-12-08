3cbc1be2cdf62c969c0aadb99978fa99d15665e4
Adam Nowak <adam.nowak@netguru.com>
2022-12-08 22:42:42 +0100

    D07p1 finally simplified
    it was hard!!! I overcomplicated things along the way, then the tests were ok, but result wasn't!
    nevertheless - FUN!:)

--- 2022-golang/07/07.go
+++ 2022-golang/07/07.go
@@ -11,76 +11,32 @@ import (
 
 const cuttingPoint = 100000
 
-type File struct {
-	parentName string
-	name       string
-	size       int
-}
-
-func createFile(parent, name string, size int) File {
-	return File{parentName: parent, name: name, size: size}
-}
-
-func RecursiveSum(parent string, list map[string]File) int {
-	var total int
-	for _, v := range list {
-		if v.parentName == parent {
-			if v.size == 0 {
-				total = RecursiveSum(v.name, list)
-			}
-			total += v.size
-		}
-	}
-	return total
-}
-
 func PartOne(stringsArray []string) (result int) {
-	allFiles := make(map[string]File)
-	allFiles["/"] = File{name: "/", parentName: "", size: 0}
-	parentName := "/"
+	sizes := []int{0}
 	for i := 2; i < len(stringsArray); i++ {
 		var cmd = stringsArray[i]
 		if cmd == "$ cd .." {
-			parentName = allFiles[parentName].parentName
+			dirSize := sizes[len(sizes)-1]
+			if dirSize < cuttingPoint {
+				result += dirSize
+			}
+			sizes = sizes[:len(sizes)-1]
+			sizes[len(sizes)-1] += dirSize
+
 		} else if cmd[0:4] == "$ cd" {
-			name := strings.Split(cmd, " ")[2]
-			allFiles[name] = createFile(parentName, name, 0)
-			parentName = name
+			sizes = append(sizes, 0)
 			i += 1
 		} else {
 			split := strings.Split(cmd, " ")
 			var size int
-			var name = split[1]
 			if split[0] != "dir" {
 				size, _ = strconv.Atoi(split[0])
 			}
-			allFiles[name] = createFile(parentName, name, size)
+			sizes[len(sizes)-1] += size
 		}
 	}
-	var sums = make(map[string]int)
 
-	tmp := make(map[string]File)
-	for k, v := range allFiles {
-		if v.size >= cuttingPoint {
-
-		} else {
-			tmp[k] = v
-		}
-
-	}
-	for _, v := range tmp {
-		if v.size <= cuttingPoint {
-			sums[v.name] = RecursiveSum(v.name, tmp)
-		}
-
-	}
-	var total int
-	for _, sum := range sums {
-		if sum <= cuttingPoint {
-			total += sum
-		}
-	}
-	return total
+	return result
 }
 
 func PartTwo(stringsArray []string) (result int) {

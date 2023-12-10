d809b04f1a5512a62f834ce8b4f05f71ba6bcbb0
Adam Nowak <lubieniebieski@gmail.com>
2023-12-11 00:20:56 +0100

    day 10 part 1

---
+++ 2023-golang/10/10.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/lubieniebieski/advent-of-code/2023-golang/tools"
+)
+
+func PartOne(input string) (result int) {
+	pipeMap := ParsePipePoints(input)
+	var start PipePoint
+	for _, point := range pipeMap {
+		for _, pipe := range point {
+			if pipe.IsStart() {
+				start = pipe
+			}
+		}
+	}
+	paths := [][]PipePoint{}
+	choices := start.WhereDoIStart(pipeMap)
+	for _, choice := range choices {
+		paths = append(paths, choice.GoToStart(start, pipeMap))
+	}
+
+	return len(paths[0])/2 + 1
+}
+
+func PartTwo(input string) (result int) {
+
+	return result
+}
+
+func parsedData() string {
+	rawData, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	return string(rawData)
+}
+
+func main() {
+	data := parsedData()
+	start := time.Now()
+	fmt.Printf("Part One: %d \n", PartOne(data))
+	fmt.Println(time.Since(start))
+
+	start = time.Now()
+	fmt.Printf("Part Two: %d \n", PartTwo(data))
+	fmt.Println(time.Since(start))
+}
+
+// Main code
+type PipePoint struct {
+	x, y int
+	id   string
+}
+
+func (p PipePoint) IsStart() bool {
+	return p.id == "S"
+}
+
+func (p PipePoint) WhereDoIStart(points [][]PipePoint) (result []PipePoint) {
+	coordinatesToCheck := []struct{ x, y int }{
+		{p.x, p.y + 1},
+		{p.x, p.y - 1},
+		{p.x + 1, p.y},
+		{p.x - 1, p.y},
+	}
+
+	for _, coordinates := range coordinatesToCheck {
+		if coordinates.x >= 0 && coordinates.y >= 0 && coordinates.x < len(points) && coordinates.y < len(points[0]) {
+
+			point := points[coordinates.y][coordinates.x]
+
+			possibleJumps, err := point.PossibleJumps(points)
+			if err != nil {
+				continue
+			}
+			for _, jump := range possibleJumps {
+				if jump.IsStart() {
+					result = append(result, point)
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func (p PipePoint) PossibleJumps(pipeMap [][]PipePoint) (result []PipePoint, err error) {
+	if p.IsStart() {
+		return p.WhereDoIStart(pipeMap), nil
+	}
+	pairs := [][]int{}
+	switch p.id {
+	case "|":
+		pairs = append(pairs, []int{p.x, p.y + 1}, []int{p.x, p.y - 1})
+	case "-":
+		pairs = append(pairs, []int{p.x + 1, p.y}, []int{p.x - 1, p.y})
+	case "L":
+		pairs = append(pairs, []int{p.x + 1, p.y}, []int{p.x, p.y - 1})
+	case "J":
+		pairs = append(pairs, []int{p.x - 1, p.y}, []int{p.x, p.y - 1})
+	case "7":
+		pairs = append(pairs, []int{p.x - 1, p.y}, []int{p.x, p.y + 1})
+	case "F":
+		pairs = append(pairs, []int{p.x + 1, p.y}, []int{p.x, p.y + 1})
+	}
+	for _, pair := range pairs {
+		if pair[0] >= 0 && pair[1] >= 0 && pair[0] < len(pipeMap) && pair[1] < len(pipeMap[0]) {
+			result = append(result, pipeMap[pair[1]][pair[0]])
+		}
+	}
+	return result, err
+}
+
+func ParsePipePoints(input string) (points [][]PipePoint) {
+	stringsArr := tools.ExtractStringsFromString(input)
+
+	for y, str := range stringsArr {
+		var row []PipePoint
+		for x, char := range str {
+			row = append(row, PipePoint{x, y, string(char)})
+		}
+		points = append(points, row)
+	}
+	return points
+}
+func (p PipePoint) MoveFromPoint(origin PipePoint, pipeMap [][]PipePoint) (previous, next PipePoint) {
+	possibleJumps, err := p.PossibleJumps(pipeMap)
+	if err != nil {
+		panic(err)
+	}
+	for _, jump := range possibleJumps {
+		if jump != origin {
+			return p, jump
+		}
+	}
+	panic(fmt.Sprintf("No jump from %v to %v", origin, p))
+}
+
+func (p PipePoint) GoToStart(start PipePoint, pipeMap [][]PipePoint) (path []PipePoint) {
+	origin, next := p.MoveFromPoint(start, pipeMap)
+	for !next.IsStart() {
+		path = append(path, origin)
+		origin, next = next.MoveFromPoint(origin, pipeMap)
+	}
+
+	return path
+}
---
+++ 2023-golang/10/10_test.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"reflect"
+	"slices"
+	"testing"
+
+	"github.com/lubieniebieski/advent-of-code/2023-golang/tools/toolstest"
+)
+
+// Input for tests from Readme
+var testCases = []struct {
+	input string
+	want  int
+}{
+	{
+		"First is for fun!",
+		1337,
+	},
+	{
+		`.....
+		.S-7.
+		.|.|.
+		.L-J.
+		.....`,
+		4,
+	},
+	{
+		`..F7.
+		.FJ|.
+		SJ.L7
+		|F--J
+		LJ...`,
+		8,
+	},
+}
+
+func TestPartOne(t *testing.T) {
+	t.Run("Works for provided example", func(t *testing.T) {
+		want := testCases[1].want
+		got := PartOne(testCases[1].input)
+		toolstest.CompareWithExample(t, want, got)
+	})
+
+	t.Run("Works for provided example #2", func(t *testing.T) {
+		want := testCases[2].want
+		got := PartOne(testCases[2].input)
+		toolstest.CompareWithExample(t, want, got)
+	})
+}
+
+func TestPartTwo(t *testing.T) {
+	t.SkipNow()
+	t.Run("Works for provided example", func(t *testing.T) {
+		want := testCases[3].want
+		got := PartTwo(testCases[3].input)
+		toolstest.CompareWithExample(t, want, got)
+	})
+}
+
+func BenchmarkPartOne(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PartOne(testCases[1].input)
+	}
+}
+
+func BenchmarkPartTwo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PartTwo(testCases[1].input)
+	}
+}
+
+// Main tests below
+
+func TestParsePipePoints(t *testing.T) {
+	input := `...
+	|S|`
+	want := [][]PipePoint{
+		{
+			{0, 0, "."},
+			{1, 0, "."},
+			{2, 0, "."},
+		},
+		{
+			{0, 1, "|"},
+			{1, 1, "S"},
+			{2, 1, "|"},
+		},
+	}
+	got := ParsePipePoints(input)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("ParsePipePoints() = %v, want %v", got, want)
+	}
+}
+
+func TestPipePoint_IsStart(t *testing.T) {
+	tests := []struct {
+		name string
+		p    PipePoint
+		want bool
+	}{
+		{
+			"Returns true if id is S",
+			PipePoint{0, 0, "S"},
+			true,
+		},
+		{
+			"Returns false if id is not S",
+			PipePoint{0, 0, "L"},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.p.IsStart(); got != tt.want {
+				t.Errorf("PipePoint.IsStart() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPipePoint_WhereDoIStart(t *testing.T) {
+	t.Run("Simple example", func(t *testing.T) {
+		// F7
+		// LS
+		pipeMap := [][]PipePoint{
+			{
+				{0, 0, "F"},
+				{1, 0, "7"},
+			},
+			{
+				{0, 1, "L"},
+				{1, 1, "S"},
+			},
+		}
+		startPoint := pipeMap[1][1]
+		want := []PipePoint{
+			pipeMap[0][1],
+			pipeMap[1][0],
+		}
+		got := startPoint.WhereDoIStart(pipeMap)
+		if len(got) != len(want) {
+			t.Errorf("Length is wrong, PipePoint.WhereDoIStart() = %v, want %v", got, want)
+		}
+		for i := range got {
+			if !slices.Contains(want, got[i]) {
+				t.Errorf("PipePoint.WhereDoIStart() = %v, want %v", got, want)
+			}
+		}
+	})
+	t.Run("more complicated example", func(t *testing.T) {
+		pipePoints := ParsePipePoints(testCases[2].input)
+		start := pipePoints[2][0]
+		want := []PipePoint{
+			pipePoints[2][1],
+			pipePoints[3][0],
+		}
+		got := start.WhereDoIStart(pipePoints)
+		if len(got) != len(want) {
+			t.Errorf("Length is wrong, PipePoint.WhereDoIStart() = %v, want %v", got, want)
+		}
+		for i := range got {
+			if !slices.Contains(want, got[i]) {
+				t.Errorf("PipePoint.WhereDoIStart() = %v, want %v", got, want)
+			}
+		}
+	})
+}
+
+func TestPipePoint_MoveFromPoint(t *testing.T) {
+	pipeMap := [][]PipePoint{
+		{
+			{0, 0, "F"},
+			{1, 0, "J"},
+		},
+		{
+			{0, 1, "L"},
+			{1, 1, "S"},
+		},
+	}
+	point := pipeMap[1][0]
+	want := pipeMap[0][0]
+	previousPoint := pipeMap[1][1]
+	_, got := point.MoveFromPoint(previousPoint, pipeMap)
+	if got != want {
+		t.Errorf("PipePoint.MoveFromPoint() = %v, want %v", got, want)
+	}
+}
